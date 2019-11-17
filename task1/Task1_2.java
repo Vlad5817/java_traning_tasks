3a9d97f1fe0e2edb71128a504c77d4a017e09de0
Uladzislau Khamkou <hamk.vo@gmail.com>
2019-11-18 01:39:25 +0300

    Заменяем табы на пробелы

--- task1/Task1_2.java
+++ task1/Task1_2.java
@@ -1,12 +1,12 @@
 public class Task1_2 {
     static String formatWeight(double weight)
     {
-		long miligrams = (long) (weight * 1000000);
-		long grams = miligrams / 1000;
+        long miligrams = (long) (weight * 1000000);
+        long grams = miligrams / 1000;
         long kilos = grams / 1000;
         long tones = kilos / 1000;
         return String.format("%d тонн, %d киллограмм, %d грамм, %d милиграмм",
-        		tones, kilos%1000, grams%1000, miligrams%1000);
+                tones, kilos%1000, grams%1000, miligrams%1000);
     }
     public static void main(String args[])
     {
