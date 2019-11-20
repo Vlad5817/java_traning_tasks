4d72f2bb7e58120cc31849d2037737e90d3be0ec
Uladzislau Khamkou <hamk.vo@gmail.com>
2019-11-20 19:05:53 +0300

    повышаем читабельность

--- task2/Task2_1.java
+++ task2/Task2_1.java
@@ -1,34 +1,32 @@
 class Vector2D {
     public double x = 0, y = 0;
-
-    public Vector2D(double x, double y)
-    {
+    public Vector2D(double x, double y) {
         this.x = x; this.y = y;
     }
-    public Vector2D substract(Vector2D vec)
-    {
-        return new Vector2D(x - vec.x, y - vec.y);
+    public Vector2D(Vector2D start, Vector2D end) {
+        x = end.x - start.x;
+        y = end.y - start.y;
     }
-    public double dot(Vector2D vec)
-    {
+    public double dot(Vector2D vec) {
         return x * vec.x + y * vec.y;
     }
 }
 
-public class task2_1 {
+public class Task2_1 {
     public static void main(String[] args) {
-        Vector2D point1 = new Vector2D(3, 2),
-                point2 = new Vector2D(0, 1),
-                point3 = new Vector2D( 2, 0),
-                v1 = point1.substract(point2),
-                v2 = point1.substract(point3),
-                v3 = point2.substract(point3);
-        if(v1.x * v2.y - v1.y * v2.x == 0)
-        {
-            System.out.print("треугольник не существует");
-        } else if (v1.dot(v2) == 0 || v1.dot(v3) == 0 || v2.dot(v3) == 0)
-            System.out.print("треугольник прямоугольный");
+        Vector2D triangle[] = {
+                new Vector2D(3., 2.),
+                new Vector2D(0., 1.),
+                new Vector2D(2., 0.),
+        };
+        Vector2D AB = new Vector2D(triangle[0], triangle[1]);
+        Vector2D BC = new Vector2D(triangle[1], triangle[2]);
+        Vector2D CA = new Vector2D(triangle[2], triangle[0]);
+        if(AB.x * BC.y - AB.y * BC.x == 0) {
+            System.out.println("треугольник не существует");
+        } else if (AB.dot(BC) == 0 || BC.dot(CA) == 0 || CA.dot(AB) == 0)
+            System.out.println("треугольник прямоугольный");
         else
-            System.out.print("треугольник существует");
+            System.out.println("треугольник существует");
     }
 }
